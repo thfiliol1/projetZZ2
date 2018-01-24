d57f76620f87ba84ecd089be2f88ad875d4a8b3f
thfiliol1 <thomasfiliol@yahoo.fr>
2018-01-24 15:53:17 +0100

    dernière mise à jour

--- paho.mqtt.plugin.js
+++ paho.mqtt.plugin.js
@@ -101,51 +101,16 @@
 		
 		
 		function reconstructBase64String(chunk) {
-		    pChunk = JSON.parse(chunk);
-
-
-		    //creates a new picture object if receiving a new picture, else adds incoming strings to an existing picture 
-		 /*   if (pictures[pChunk["pic_id"]]==null) {
-			pictures[pChunk["pic_id"]] = {"count":0, "total":pChunk["size"], pieces: {}, "pic_id": pChunk["pic_id"]};
-
-			pictures[pChunk["pic_id"]].pieces[pChunk["pos"]] = pChunk["data"];
-
-		    }
-
-		    else {
-			pictures[pChunk["pic_id"]].pieces[pChunk["pos"]] = pChunk["data"];
-			pictures[pChunk["pic_id"]].count += 1;
-
-
-			if (pictures[pChunk["pic_id"]].count == pictures[pChunk["pic_id"]].total) {
-			console.log("Image reception compelete");
-			var str_image=""; 
-
-			for (var i = 0; i <= pictures[pChunk["pic_id"]].total; i++) 
-			    str_image = str_image + pictures[pChunk["pic_id"]].pieces[i];
-
-			*/
-		//jQuery('#res').html("");
+		    	pChunk = JSON.parse(chunk);		 
 			str_image = str_image + pChunk["data"];
+			//last packet
 			if(pChunk["pos"] == pChunk["size"]){
-				console.log(str_image);
 				//displays image
 				var source = 'data:image/jpeg;base64,'+str_image;
 				var html = '<img src="'+source+'"></img>';
-				//var myImageElement = document.getElementById("picture_to_show");
-				//myImageElement.src = '#';
-				//myImageElement.src = source;
 				data.msg = html;
-				console.log(html);
-		
-				//jQuery('#res').append("<img id='picture_to_show'></img>");
-				//jQuery('#picture_to_show').attr('src', source);
 				str_image="";
-
 			}
-
-		   // }
-
 		};
 
 		function onMessageArrived(message) {
